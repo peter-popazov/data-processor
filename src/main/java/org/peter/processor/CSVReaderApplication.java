4543f2671c0e77be5214d1464fc4221b22cb8ed3
Peter Popazov <popazovpeta@gmail.com>
2025-02-25 15:04:34 +0200

    init

---
+++ src/main/java/org/peter/processor/CSVReaderApplication.java
@@ -0,0 +1,12 @@
+package org.peter.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CSVReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CSVReaderApplication.class, args);
+    }
+}
---
+++ src/test/java/org/peter/processor/CsvreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.peter.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
